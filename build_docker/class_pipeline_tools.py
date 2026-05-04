b64213847645cf35d29b9372d603cee46b410ac3
HamzaTolimat <hamza@tolimat.com>
2026-05-04 16:24:39 +0200

    fix(build): rework job need string to real list

--- build_docker/class_pipeline_tools.py
+++ build_docker/class_pipeline_tools.py
@@ -42,21 +42,19 @@ def convert_multistage_parents_version_to_docker_args(multistage_parent, debug):
     return docker_args
 
 def create_job_needs(parent, multistage_parents, mode):
-    job_needs = "[{pipeline: '$PARENT_PIPELINE_ID',job: 'convert-jsonnet-to-json',}"
+    job_needs = [{'pipeline': '$PARENT_PIPELINE_ID','job': 'convert-jsonnet-to-json',}]
 
     if mode == "all" :
-        job_needs += ",{ job: '" + parent.name + ":" + parent.version + "',optional: true}"
+        job_needs.append({ 'job': f"{parent.name}:{parent.version}",'optional': 'true'})
     else :
         if not parent.external and parent.is_building :
-            job_needs += ",{ job: '" + parent.name + ":" + parent.version + "',optional: true}"
+            job_needs.append({ 'job': f"{parent.name}:{parent.version}",'optional': 'true'})
     
     job_already_here = [parent.name + ":" + parent.version]
     for multistage_parent in multistage_parents :
         job_name = multistage_parent.name + ":" + multistage_parent.version
         if not multistage_parent.external and multistage_parent.is_building and job_name not in job_already_here:
-            job_needs += ",{ job: '" + job_name + "',optional: true}"
+            job_needs.append({ 'job': job_name,'optional': 'true'})
             job_already_here.append(job_name)
-
-    job_needs += "]"
 
     return job_needs
