64c80ec4223ca019d61bed366037723a37135974
Antonin Sibileau <antonin.sibileau@i-carre.net>
2026-05-04 15:36:25 +0200

    Optimising to o(n) and getting english comments

--- detect_debt/main.py
+++ detect_debt/main.py
@@ -6,8 +6,6 @@ logger = logging.getLogger(__name__)
 
 def main(args) : 
    
-    # Optimimser la feature pour ne checker que les nouvelles images / les changements dans les commits ?
-    
     logger.info(f"[General] Scanning {args.path} to find Dockerfiles")
 
     dockerfiles = find_dockerfiles_r(args.current_repo, args.path)
@@ -16,49 +14,34 @@ def main(args) :
     
     description = ""
 
-    for df in dockerfiles:
-        logger.debug(df)
+    # Creating a dict of all dockerfiles with their versions
+    versions = {}
+    for x in dockerfiles:
+        if x.name not in versions:
+            versions[x.name] = []
+        versions[x.name].append(x.path.split('/')[-1])
 
-    # Analyse la dette   
+    # Debt analysis   
     for df in dockerfiles:
-        # On check que les dockerfiles dont les parents sont dans notre repo
+        # Check only dockerfiles which depend on internal parent
         if not df.parent.external:
-            # On regarde s'il existe une version plus récente du parent
-            parents = [x for x in dockerfiles if x.name == df.parent.name] # o(n²) c'est pas très propre mais c'est la vie
-
-# claude qui me passe une solution smart en o(n), construire une map de version avant la boucle
-# versions = {}
-# for x in dockerfiles:
-#     if x.name not in versions:
-#         versions[x.name] = []
-#     versions[x.name].append(x.path.split('/')[-1])
-
-# et puis
-# latest = max(versions[df.parent.name])
-
-            latest = max([x.path.split('/')[-1] for x in parents])
+            latest = max(versions[df.parent.name])
             if df.parameters.parent_version['version_number'] < latest :
                 logger.debug(f"Found technical debt for {df.name} at {df.path}, using parent {df.parent.name} {df.parameters.parent_version['version_number']} but could be using version {latest}")
                 description += f"{df.path}, using parent {df.parent.name} {df.parameters.parent_version['version_number']} but could be using version {latest}\n"
 
-    # Crée ou modifie l'issue de la dette technique
-    
+    # Creating/modifying debt issue
     payload = {'title' : 'Technical debt', 'description' : description, 'labels' : 'En développement', 'assignee_id' : 9401}
     logger.info(f"Payload created : {payload}")
 
     issue_filter = {'search': 'Technical debt'}
 
     obtained_issues = get_issues(args.token, args.project_id, issue_filter)
-    logger.debug(f"Obtained issues : {obtained_issues}")
 
     if not obtained_issues:
-        logger.debug("Creating issue ...")
         created_issue = create_issue(args.token, args.project_id, payload)
-        logger.debug(f"Created issue : {created_issue}")
     else:
-        logger.debug("Updating issue ...") 
         updated_issue = update_issue(args.token, args.project_id, obtained_issues[0]["iid"], payload)
-        logger.debug(f"Updated issue : {updated_issue}")
 
 #=======================================================#
 #====================== Arguments ======================#
