329cddb7b99fe34d930f13562dc842f9a9082b03
Antonin Sibileau <antonin.sibileau@i-carre.net>
2026-05-05 10:25:32 +0200

    bon en fait pas besoin du assigneeS

--- detect_debt/global_vars.py
+++ detect_debt/global_vars.py
@@ -6,4 +6,4 @@ from lib.global_vars import *
 DETECT_DEBT_LOG_LEVEL_DEFAULT = "DEBUG"
 DETECT_DEBT_ISSUE_TITLE_DEFAULT = "[Technical debt]"
 DETECT_DEBT_ISSUE_LABEL_DEFAULT = "En développement"
-DETECT_DEBT_ISSUE_ASSIGNEE_USERNAME_DEFAULT = "antonin.sibileau,hamza.tolimat.i"
+DETECT_DEBT_ISSUE_ASSIGNEE_USERNAME_DEFAULT = "antonin.sibileau"
