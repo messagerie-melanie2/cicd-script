b0912fbac3952981a40dd841431e7131c2c25985
Antonin Sibileau <antonin.sibileau@i-carre.net>
2026-05-05 16:31:01 +0200

    fix(detect-debt): trying to understand logging behaviour bis

--- detect_debt/main.py
+++ detect_debt/main.py
@@ -1,6 +1,6 @@
+from detect_debt.global_vars import *
 from build_docker.find_dockerfiles import find_dockerfiles_r
 from lib.gitlab_helper import get_issues, create_issue, get_issues, update_issue, get_user_id, get_users
-from detect_debt.global_vars import *
 
 logger = logging.getLogger(__name__)
 
