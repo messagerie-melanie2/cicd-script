9a798c2c89266cbb510d93d2782d462e88036875
HamzaTolimat <hamza@tolimat.com>
2026-05-05 10:51:24 +0200

    feat(clean): Add more option for keeping tags

--- clean_registry/clean_dev.py
+++ clean_registry/clean_dev.py
@@ -1,5 +1,6 @@
 from clean_registry.global_vars import *
 from lib.gitlab_helper import get_branches, get_tags_in_repository,delete_tag_in_repository
+from clean_registry.clean_tools import check_if_is_tag_to_keep
 
 logger = logging.getLogger(__name__)
 
@@ -7,17 +8,19 @@ def clean_dev_images(registry,token,project_id):
     dev_tags_to_delete = []
 
     branches = get_branches(token,project_id)
+    logger.debug(f"branches of project : {branches}")
 
     for repository in registry :
+        logger.debug(f"repository : {repository}")
         tags = get_tags_in_repository(token,project_id,repository["id"])
+        logger.debug(f"tags : {tags}")
         for tag in tags :
             is_not_current_dev_tag = True
             for branch in branches:
                 if branch["name"] in tag["name"]:
                     is_not_current_dev_tag = False
-            
-            #Recette is not a branch in mel_docker
-            if RECETTE_KEY in tag["name"]:
+
+            if check_if_is_tag_to_keep(tag["name"]):
                 is_not_current_dev_tag = False
             
             if is_not_current_dev_tag :
@@ -30,10 +33,10 @@ def clean_dev_images(registry,token,project_id):
     for tag in dev_tags_to_delete :
         if tag["repository_name"] not in REPOSITORIES_WHITELIST :
             logger.info(f"DEV : we have to delete {tag['image_name']} tag")
-            deleted = delete_tag_in_repository(token,project_id,tag["repository_id"],tag["name"])
-            if deleted :
-                logger.info(f"{tag['image_name']} tag is deleted")
-            else :
-                logger.warning(f"{tag['image_name']} tag couldn't be deleted")
+            # deleted = delete_tag_in_repository(token,project_id,tag["repository_id"],tag["name"])
+            # if deleted :
+            #     logger.info(f"{tag['image_name']} tag is deleted")
+            # else :
+            #     logger.warning(f"{tag['image_name']} tag couldn't be deleted")
         else :
             logger.info(f"{tag['image_name']} tag is whitelisted")
--- clean_registry/clean_no_build.py
+++ clean_registry/clean_no_build.py
@@ -1,5 +1,6 @@
 from clean_registry.global_vars import *
 from lib.gitlab_helper import get_branches, get_tags_in_repository,delete_repository_in_registry,delete_tag_in_repository
+from clean_registry.clean_tools import check_if_is_dev_branch, check_if_is_tag_to_keep
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +27,7 @@ def filter_ghost_tags_with_no_dev_branch(branches,ghost_tags_with_dev_branch):
     for tag in ghost_tags_with_dev_branch :
         is_not_dev_tag = True
         for branch in branches:
-            if branch["name"] != PREPROD_KEY and branch["name"] != PROD_KEY and RECETTE_KEY not in tag["name"]:
+            if check_if_is_dev_branch(branch) and not check_if_is_tag_to_keep(tag["name"]):
                 if branch["name"] in tag["name"]:
                     is_not_dev_tag = False
         
@@ -41,37 +42,45 @@ def clean_ghost_images(registry,dockerfiles_to_build,token,project_id):
     ghost_tags = []
 
     branches = get_branches(token,project_id)
-
+    logger.debug(f"branches of project : {branches}")
     for repository in registry :
+        logger.debug(f"repository : {repository}")
+
         tags = get_tags_in_repository(token,project_id,repository["id"])
+        logger.debug(f"tags : {tags}")
 
         ghost_tags_with_dev_branch,repository_not_present = filter_ghost_tags_with_dev_branch(dockerfiles_to_build,repository,tags)
+        logger.debug(f"ghost_tags_with_dev_branch : {ghost_tags_with_dev_branch}")
 
         ghost_tags_no_dev_branch = filter_ghost_tags_with_no_dev_branch(branches,ghost_tags_with_dev_branch)
+        logger.debug(f"ghost_tags_no_dev_branch : {ghost_tags_no_dev_branch}")
 
         ghost_tags += ghost_tags_no_dev_branch
         
         if (repository_not_present and len(ghost_tags_no_dev_branch) > 0) or (repository_not_present and len(ghost_tags_with_dev_branch) == 0) :
             ghost_repositories.append(repository)
     
+    logger.debug(f"ghost_tags : {ghost_tags_with_dev_branch}")
+    logger.debug(f"ghost_repositories : {ghost_repositories}")
+
     for ghost_repository in ghost_repositories :
         if ghost_repository["name"] not in REPOSITORIES_WHITELIST :
             logger.info(f"we have to delete {ghost_repository['name']} repository")
-            deleted = delete_repository_in_registry(token,project_id,ghost_repository["id"])
-            if deleted :
-                logger.info(f"{ghost_repository['name']} repository is deleted")
-            else :
-                logger.warning(f"{ghost_repository['name']} repository couldn't be deleted")
+            # deleted = delete_repository_in_registry(token,project_id,ghost_repository["id"])
+            # if deleted :
+            #     logger.info(f"{ghost_repository['name']} repository is deleted")
+            # else :
+            #     logger.warning(f"{ghost_repository['name']} repository couldn't be deleted")
         else :
             logger.info(f"{ghost_repository['name']} repository is whitelisted")
     
     for ghost_tag in ghost_tags :
         if ghost_tag["repository_name"] not in REPOSITORIES_WHITELIST :
             print(f"we have to delete {ghost_tag['image_name']} tag")
-            deleted = delete_tag_in_repository(token,project_id,ghost_tag["repository_id"],ghost_tag["name"])
-            if deleted :
-                logger.info(f"{ghost_tag['image_name']} tag is deleted")
-            else :
-                logger.warning(f"{ghost_tag['image_name']} tag couldn't be deleted")
+            # deleted = delete_tag_in_repository(token,project_id,ghost_tag["repository_id"],ghost_tag["name"])
+            # if deleted :
+            #     logger.info(f"{ghost_tag['image_name']} tag is deleted")
+            # else :
+            #     logger.warning(f"{ghost_tag['image_name']} tag couldn't be deleted")
         else :
             logger.info(f"{ghost_tag['image_name']} tag is whitelisted")
