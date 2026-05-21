e26466518d1a78d655fb567a234e600d2b24f694
Antonin Sibileau <antonin.sibileau@i-carre.net>
2026-05-21 15:28:36 +0200

    feat(detect-debt externe): adding external / internal boolean activation

--- detect_debt/detect_debt_function.py
+++ detect_debt/detect_debt_function.py
@@ -86,11 +86,11 @@ def get_info_from_dockerhub(current_name, current_version, latest = "latest") ->
    
     if "/" in current_name:
         parts = current_name.split("/")
+        current_name = parts[-1]
         if "." in parts[0]:  # registry prefix that we ignore (docker.io)
             namespace = "library"
         else:
             namespace = parts[0]
-        current_name = parts[-1]
     else:
         namespace = "library"
 
@@ -165,7 +165,7 @@ def get_external_debt_description(sorted_dockerfiles) -> str:
                 if current_version_in_latest :
                     description_dirty += f"{df.path} | {df.parent.version} | {', '.join(current_tags)} | {', '.join(latest_tags)}\n"   
             else :
-                description_404 += f"{df.path} | {df.parent.version}\n"
+                description_404 += f"{df.path} | {df.parent.name} {df.parent.version}\n"
     
     description += "## Distrib hors standard\n"
     description += description_dirty
--- detect_debt/main.py
+++ detect_debt/main.py
@@ -10,12 +10,14 @@ def main(args) :
 
     obtained_dockerfiles = find_dockerfiles_r(args.current_repo, args.path)
     
-    payload_internal_debt, issue_filter_internal_debt = internal_debt(args.token, args.project_id, obtained_dockerfiles)
+    if DETECT_INTERNAL_DEBT_ACTIVATE_JOB :
+        payload_internal_debt, issue_filter_internal_debt = internal_debt(args.token, args.project_id, obtained_dockerfiles)
 
     if DETECT_INTERNAL_DEBT_ACTIVATE_ISSUE : 
         create_or_update_issue(args.token, args.project_id, payload_internal_debt, issue_filter_internal_debt)
 
-    payload_external_debt, issue_filter_external_debt = external_debt(args.token, args.project_id, obtained_dockerfiles)
+    if DETECT_EXTERNAL_DEBT_ACTIVATE_JOB : 
+        payload_external_debt, issue_filter_external_debt = external_debt(args.token, args.project_id, obtained_dockerfiles)
    
     if DETECT_EXTERNAL_DEBT_ACTIVATE_ISSUE : 
         create_or_update_issue(args.token, args.project_id, payload_external_debt, issue_filter_external_debt)
